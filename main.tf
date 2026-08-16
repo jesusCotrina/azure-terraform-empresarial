6a2b550395157fe288d1f630fea49a571049e5b8
Jesus Cotrina <polopanntalon2@hotmail.com>
2026-08-16 10:43:20 -0500

    deploy resource

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "stterraformjc001"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+
+    use_azuread_auth = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  resource_provider_registrations = "none"
+}
---
+++ resources.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "demo" {
+  name     = "rg-terraform-github-demo"
+  location = "East US"
+}
